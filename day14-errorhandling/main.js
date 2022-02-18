731277195e9e5946ddf550d16e1169bc968ba547
uriel g <urigalarza@hotmail.com>
2022-02-17 19:36:21 -0500

    completed day 14

---
+++ day14-errorhandling/main.js
@@ -0,0 +1,30 @@
+try {
+let lastName = 'Yetayeh'
+let fullName = fistName + ' ' + lastName
+} catch (err) {
+console.log('Name of the error', err.name)
+console.log('Error message', err.message)
+} finally {
+console.log('In any case I will be executed')
+}
+
+const throwErroExampleFun = () => {
+let message
+let x = prompt('Enter a number: ')
+try {
+    if (x == '') throw 'empty'
+    if (isNaN(x)) throw 'not a number'
+    x = Number(x)
+    if (x < 5) throw 'too low'
+    if (x > 10) throw 'too high'
+} catch (err) {
+    console.log(err)
+}
+}
+throwErroExampleFun()
+
+
+let square = 2*2
+console.log(square)
+
+console.log('Hello, world')
