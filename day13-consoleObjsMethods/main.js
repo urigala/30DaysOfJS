1e4b816a547f5123447d3cd01c1b3b47a3b916d2
uriel g <urigalarza@hotmail.com>
2022-02-17 19:19:45 -0500

    completed day 13

---
+++ day13-consoleObjsMethods/main.js
@@ -0,0 +1,18 @@
+console.log('%c30 Days Of JavaScript', 'color:green') // log output is green
+console.log(
+  '%c30 Days%c %cOf%c %cJavaScript%c',
+  'color:green',
+  '',
+  'color:red',
+  '',
+  'color:yellow'
+) // log output green red and yellow text
+
+const user = {
+    name: 'Asabeneh',
+    title: 'Programmer',
+    country: 'Finland',
+    city: 'Helsinki',
+    age: 250
+  }
+  console.table(user)
